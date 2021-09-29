0b0eb103daab6c6142871502533be5668261e4a1
Ravi Bhoot <bhoot@google.com>
2021-09-29 13:24:18 +0530

    temp commit for column map validator

---
+++ spec_validator/column_map_validator.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+column_map_path = '../spec_dir/column_maps/column_map.json'
+
+os.path.expanduser(column_map_path)
+column_map = json.load(open(column_map_path, 'r'))
+
+stat_dir = {}
+dcid_list_all = {}
+for year in column_map:
+	stat_dir[year] = {}
+	stat_dir[year]['actual_column_count'] = len(column_map[year])
+	
+	moe_stats_count = 0
+	estimate_stat_count = 0
+	dcid_list = {}
+	
+	# TODO column should have been ignored
+	
+	for column in column_map[year]:
+		if column_map[year][column]['Node'] not in dcid_list:
+			dcid_list[column_map[year][column]['Node']] = []
+		dcid_list[column_map[year][column]['Node']].append(column)
+		# margin of error and normal statvar counts
+		if column_map[year][column]['statType'] == "dcid:marginOfError":
+			moe_stats_count += 1
+		else:
+			estimate_stat_count += 1
+	
+	dcid_list_all[year] = dcid_list
+	
+	stat_dir[year]['estimate_count'] = estimate_stat_count
+	stat_dir[year]['moe_count'] = moe_stats_count
+	# stat_dir[year]['dcid_list'] = dcid_list
+	stat_dir[year]['repeated_dcids'] = {}
+	for dcid in dcid_list:
+		if len(dcid_list[dcid]) > 1:
+			stat_dir[year]['repeated_dcids'].update({dcid: dcid_list[dcid]})
+	
+	# TODO missing columns in output
+
+
+print(json.dumps(list(set(dcid_list_all['2016'])-set(dcid_list_all['2019'])), indent=2))
+
+
+json.dump(stat_dir, open('./outputs/column_map_stats.json', 'w'), indent=2)
+# print(json.dumps(stat_dir, indent=2))
