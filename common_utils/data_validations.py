431294ff0023c234ded58d68e29af4a584afb375
Ravi Bhoot <bhoot@google.com>
2021-10-06 23:57:40 +0530

    add new script for handling 2500- values

---
+++ common_utils/data_validations.py
@@ -0,0 +1,23 @@
+import csv 
+import os
+import re
+
+
+csv_path = os.path.expanduser('~/Documents/acs_tables/S0701/run2/S0701_cleaned.csv')
+out_csv_path = os.path.expanduser('~/Documents/acs_tables/S0701/run2/S0701_cleaned_fixed.csv')
+
+csv_reader = csv.reader(open(csv_path, 'r'))
+csv_writer = csv.writer(open(out_csv_path, 'w'))
+
+for row in csv_reader:
+	if csv_reader.line_num == 1:
+		csv_writer.writerow(row)
+	else:
+		try:
+			float(row[3])
+			csv_writer.writerow(row)
+		except ValueError:
+			print(','.join(row))
+			row[3] = re.sub("[^0-9.]", "", row[3])
+			csv_writer.writerow(row)
+			# print(row[3])
