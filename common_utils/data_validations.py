638db8fbd00846a4fb0bcbd0f5990dec6040e368
Ravi Bhoot <bhoot@google.com>
2021-10-21 01:15:18 +0530

    allow column name in data validation

--- common_utils/data_validations.py
+++ common_utils/data_validations.py
@@ -4,40 +4,40 @@ import re
 from absl import flags, app
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('csv_path', '~/Documents/acs_tables/S0701/run2/S0701_cleaned.csv', 'Path to the input csv')
-flags.DEFINE_string('out_csv_path', '~/Documents/acs_tables/S0701/run2/S0701_cleaned_fixed.csv','Path to the fixed, clean csv')
-flags.DEFINE_string('log_csv_path', '~/Documents/acs_tables/S0701/run2/S0701_fix_required.csv','Path to the csv log')
+flags.DEFINE_string('csv_path', None, 'Path to the input csv')
+flags.DEFINE_string('column_tag', 'Quantity', 'Column name of the column with values to be checked')
 
 def fix_open_distributions_in_observation_csv(argv):
 	"""
 	Function to fix the open distributions in the StatVarObservation values
 	"""
-	# csv_path = os.path.expanduser('~/Documents/acs_tables/S0701/run2/S0701_cleaned.csv')
-	# out_csv_path = os.path.expanduser('~/Documents/acs_tables/S0701/run2/S0701_cleaned_fixed.csv')
-	# log_csv_path = os.path.expanduser('~/Documents/acs_tables/S0701/run2/S0701_fix_required.csv')
 
-	csv_path = os.path.expanduser(FLAGS.csv_path)
-	out_csv_path = os.path.expanduser(FLAGS.out_csv_path)
-	log_csv_path = os.path.expanduser(FLAGS.log_csv_path)
+	csv_path = os.path.abspath(os.path.expanduser(FLAGS.csv_path))
+	out_csv_path = csv_path.replace('.csv', '_fixed.csv')
+	log_csv_path = csv_path.replace('.csv', '_fix_log.csv')
 	
 	csv_reader = csv.reader(open(csv_path, 'r'))
 	csv_writer = csv.writer(open(out_csv_path, 'w'))
 	log_csv_writer = csv.writer(open(log_csv_path, 'w'))
 
+	column_index = 3
 	for row in csv_reader:
 		if csv_reader.line_num == 1:
 			csv_writer.writerow(row)
 			log_csv_writer.writerow(row)
+			if FLAGS.column_tag in row:
+				column_index = row.index(FLAGS.column_tag)
 		else:
 			try:
-				float(row[3])
+				float(row[column_index])
 				csv_writer.writerow(row)
 			except ValueError:
 				print(','.join(row))
 				log_csv_writer.writerow(row)
-				row[3] = re.sub("[^0-9.]", "", row[3])
+				row[column_index] = re.sub("[^0-9.]", "", row[column_index])
 				csv_writer.writerow(row)
 				# print(row[3])
 
 if __name__ == '__main__':
-  app.run(fix_open_distributions_in_observation_csv)
+	flags.mark_flags_as_required(['csv_path'])
+	app.run(fix_open_distributions_in_observation_csv)
